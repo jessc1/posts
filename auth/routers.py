969c4f1e1906dafe9cb2e4defe93c1f539050829
root <root@DESKTOP-5CO6E6Q.localdomain>
2025-09-05 15:24:12 -0300

    including user login

--- auth/routers.py
+++ auth/routers.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 from auth.viewsets.register import RegisterViewSet
+from auth.viewsets.login import LoginViewSet
 
 router = routers.SimpleRouter()
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 urlpatterns = [
     *router.urls,
