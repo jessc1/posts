e9caa2392b9eb6e8bc333f7e00fb1d85baeda8f8
root <root@DESKTOP-5CO6E6Q.localdomain>
2025-09-10 16:29:01 -0300

    logout and test for logout

--- auth/routers.py
+++ auth/routers.py
@@ -2,11 +2,14 @@ from rest_framework import routers
 from auth.viewsets.register import RegisterViewSet
 from auth.viewsets.login import LoginViewSet
 from auth.viewsets.refresh import RefreshViewSet
+from auth.viewsets.logout import LogoutViewSet
 
 router = routers.SimpleRouter()
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 router.register(r'auth/login', LoginViewSet, basename='auth-login')
 router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+router.register(r'auth/logout', LogoutViewSet, basename='auth-logout')
+
 
 
 
--- auth/tests.py
+++ auth/tests.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework import status
 from fixtures.user import user
+from configtest import client
 
 class TestAuthenticationViewSet:
     endpoint = '/api/auth/'
@@ -40,3 +41,17 @@ class TestAuthenticationViewSet:
         response = client.post(self.endpoint + "refresh/", data_refresh)
         assert response.status_code == status.HTTP_200_OK
         assert response.data['access']
+    
+    def test_logout(self, client, user):
+        data = {
+            "username": user.username,
+            "password": "ieiepwd"
+        }
+        response = client.post(self.endpoint + "login/", data)
+        assert response.status_code == status.HTTP_200_OK
+        client.force_authenticate(user=user)
+        data_refresh = {"refresh": response.data["refresh"]}
+        response = client.post(self.endpoint + "logout/", data_refresh)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
