c48e9a669d9631925f67dd84ca02a009dc73289c
root <root@DESKTOP-5CO6E6Q.localdomain>
2025-09-12 15:41:43 -0300

    update UserPermission

--- user/viewsets.py
+++ user/viewsets.py
@@ -1,17 +1,19 @@
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from abstract.viewsets import AbstractViewSet
 from user.serializers import UserSerializer
 from user.models import User
+from django.core.cache import cache
+from auth.permissions import UserPermission
+
 
 class UserViewSet(AbstractViewSet):
     http_method_names = ('patch',  'get')
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (UserPermission,)
     serializer_class = UserSerializer
 
     def get_queryset(self):
         if self.request.user.is_superuser:
-            return User.objects.all()
+            return User.objects.all()  
         return User.objects.exclude(is_superuser=True)
     
     def get_object_by_id(self):
